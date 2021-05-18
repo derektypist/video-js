6a8d5edbae7249e03726256ec3ac786969145781
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-05-18 16:45:58 +0000

    Added event listener to preloader at script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,16 @@ const video = document.querySelector('.video-container');
 btn.addEventListener('click', function() {
     if (!btn.classList.contains('slide')) {
         btn.classList.add('slide');
+        video.pause();
     } else {
         btn.classList.remove('slide');
+        video.play();
     }
+});
+
+// Set Up preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load',function() {
+    preloader.classList.add("hide-preloader");
 });
